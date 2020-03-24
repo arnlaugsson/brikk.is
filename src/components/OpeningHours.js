614228d5c8668b226fbe452cd846987b8ec0ec03
Skúli Arnlaugsson <arnlaugsson@users.noreply.github.com>
2020-03-24 00:13:32 +0000

    Covid19 temporary change
    Message from Brikk on Facebook:
    Kæru Brikk vinir
    
    Staðan er fljót að breytast þessa dagana og við aðlögum okkur að nýjum aðstæðum.
    
    > Frá og með morgundeginum 24. mars verðum við með tímabundinn breyttan opnunartíma og verður opið alla daga frá 9-16. 
    > Við munum eingöngu bjóða upp á take-away svo ekki verður hægt að borða á staðnum.
    > Á næstu dögum munum við svo bjóða upp á heimsendingar og munum við auglýsa það vel.  
    > Farið vel með ykkur kæru vinir, verum góð hvort við annað.
    > Kær kveðja, Brikk ❤️

--- src/components/OpeningHours.js
+++ src/components/OpeningHours.js
@@ -20,25 +20,25 @@ function OpeningHours(props) {
       </h3>
       <ul className="opening-hours">
         <li className={isToday(0)}>
-          {t("sundays")} <span className="pull-right">9:00-17:00</span>
+          {t("sundays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(1)}>
-          {t("mondays")} <span className="pull-right">7:30-17:00</span>
+          {t("mondays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(2)}>
-          {t("tuesdays")} <span className="pull-right">7:30-17:00</span>
+          {t("tuesdays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(3)}>
-          {t("wednesdays")} <span className="pull-right">7:30-17:00</span>
+          {t("wednesdays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(4)}>
-          {t("thursdays")} <span className="pull-right">7:30-17:00</span>
+          {t("thursdays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(5)}>
-          {t("fridays")} <span className="pull-right">7:30-17:00</span>
+          {t("fridays")} <span className="pull-right">9:00-16:00</span>
         </li>
         <li className={isToday(6)}>
-          {t("saturdays")} <span className="pull-right">9:00-17:00</span>
+          {t("saturdays")} <span className="pull-right">9:00-16:00</span>
         </li>
       </ul>
     </div>
